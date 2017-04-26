3f0f82aeb9e6c52a3512e375dd49bb3a01140a06
Administrator <Administrator@shudalei-PC>
2017-04-26 17:30:11 +0800

    add pageUtil

--- src/main/java/com/moxi/controller/NewsController.java
+++ src/main/java/com/moxi/controller/NewsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.moxi.model.NewsCategory;
 import com.moxi.service.NewsCategoryService;
+import com.moxi.util.PageUtil;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,32 +24,23 @@ public class NewsController {
 		//判断
 		if(pageSize == 0) pageSize = 10;
 		if(pageCurrent == 0) pageCurrent = 1;
-		if(pageCount == 0) pageCount = newsCategoryService.count(newsCategory);
-
+		int rows = newsCategoryService.count(newsCategory);
+		if(pageCount == 0) pageCount = rows%pageSize == 0 ? (rows/pageSize) : (rows/pageSize) + 1;
+		
 		//查询
 		newsCategory.setStart((pageCurrent - 1)*pageSize);
 		newsCategory.setEnd(pageSize);
 		List<NewsCategory> list = newsCategoryService.list(newsCategory);
 		
 		//输出
-		model.addAttribute("pageCurrent", pageCurrent);
-		model.addAttribute("pageCount", pageCount);
-		model.addAttribute("pageSize", pageSize);
-		model.addAttribute("test", "<li class=\"footable-page-arrow\"><a href=\"newsCategoryManage_0_0_0\">«</a></li>");
 		model.addAttribute("list", list);
+		String pageHTML = PageUtil.getPageContent("newsCategoryManage_{pageCurrent}_{pageSize}_{pageCount}?name="+newsCategory.getName(), pageCurrent, pageSize, pageCount);
+		model.addAttribute("pageHTML",pageHTML);
 		return "/news/newsCategoryManage";
 	}
 	
-	
-	
-	
-	
-	
-	
-	
 	@RequestMapping("/newsManage_{pageSize}_{currentPage}")
 	public String newsManage(Model model, @PathVariable Integer pageSize, @PathVariable Integer currentPage) {
-
 		return "/news/newsManage";
 	}
 }
--- src/main/java/com/moxi/util/PageUtil.java
+++ src/main/java/com/moxi/util/PageUtil.java
@@ -1,48 +1,29 @@
 package com.moxi.util;
 
 public class PageUtil {
-
 	
-	public static String getPageContent(String url,int pageCount,int pageCurrent){
+	public static String getPageContent(String url,int pageCurrent,int pageSize,int pageCount){
+		if (pageCount == 0) {
+			return "";
+		}
+		String urlNew = url.replace("{pageSize}", pageSize+"").replace("{pageCount}", pageCount+"");
 		
-		
-		String first = url.replace("%page%", 0+"");
-		String prev = url.replace("%page%", pageCurrent-1+"");
-		
-		String last = url.replace("%page%", 0+"");
-		String next = url.replace("%page%", 0+"");
-		
+		String first = urlNew.replace("{pageCurrent}", 1+"");
+		String prev = urlNew.replace("{pageCurrent}", (pageCurrent - 1)+"");
+		String next = urlNew.replace("{pageCurrent}", (pageCurrent + 1)+"");
+		String last = urlNew.replace("{pageCurrent}", pageCount+"");
 		
 		StringBuffer html = new StringBuffer();
-		
-		html.append("<li class=\"footable-page-arrow"+(pageCurrent<=1?" disabled":"")+"\"><a href=\""+first+"\">«</a></li>");
-		html.append("<li class=\"footable-page-arrow"+(pageCurrent<=1?" disabled":"")+"\"><a href=\"#first\">‹</a></li>");
-		
+		html.append("<li class=\"footable-page-arrow"+(pageCurrent<=1?" disabled":"")+"\"><a href=\""+(pageCurrent<=1?"#":first)+"\">«</a></li>");
+		html.append("<li class=\"footable-page-arrow"+(pageCurrent<=1?" disabled":"")+"\"><a href=\""+(pageCurrent<=1?"#":prev)+"\">‹</a></li>");
 		for(int i = 0 ;i < pageCount; i++){
-			
-			html.append("<li class=\"footable-page-arrow\"><a href=\"#first\">«</a></li>");
+			String urlItem = urlNew.replace("{pageCurrent}", (i+1)+"");
+			html.append("<li class=\"footable-page"+(((i+1) == pageCurrent)?" active":"")+"\"><a href=\""+urlItem+"\">"+(i+1)+"</a></li>");
 		}
+		html.append("<li class=\"footable-page-arrow"+(pageCurrent==pageCount?" disabled":"")+"\"><a href=\""+(pageCurrent==pageCount?"#":next)+"\">›</a></li>");
+		html.append("<li class=\"footable-page-arrow"+(pageCurrent==pageCount?" disabled":"")+"\"><a href=\""+(pageCurrent==pageCount?"#":last)+"\">»</a></li>");
 		
-		html.append("<li class=\"footable-page-arrow\"><a href=\"#first\">«</a></li>");
-		html.append("<li class=\"footable-page-arrow\"><a href=\"#first\">«</a></li>");
-		
-		
-		
-		
-		
-		return "";
+		return html.toString().replaceAll("null", "");
 	}
 	
 }
-
-
-//<li class="footable-page-arrow disabled"><a href="#first">«</a></li>
-//<li class="footable-page-arrow"><a href="#prev">‹</a></li>
-//<li class="footable-page"><a href="#">1</a></li>
-//<li class="footable-page active"><a href="#">2</a></li>
-//<li class="footable-page"><a href="#">3</a></li>
-//<li class="footable-page"><a href="#">4</a></li>
-//<li class="footable-page"><a href="#">5</a></li>
-//<li class="footable-page-arrow"><a href="#next">›</a></li>
-//<li class="footable-page-arrow"><a href="#last">»</a></li>
-
